690e5187c7a77d1882c0c8f9bd9abf24c728f114
Cristiano <cristianovelkan @gmail.com>
2022-07-12 07:18:58 -0300

    feat: firebase login user

--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,35 @@
 import axios from "axios";
 import { AUTH_URL, AUTH_KEY } from "../config";
 
-export async function createUser(email, password) {
+export async function authenticate(
+  email,
+  password,
+  mode = "signInWithPassword"
+) {
   try {
-    const response = await axios.post(`${AUTH_URL}:signUp?key=${AUTH_KEY}`, {
+    console.log(`${AUTH_URL}:${mode}?key=${AUTH_KEY}`);
+    const response = await axios.post(`${AUTH_URL}:${mode}?key=${AUTH_KEY}`, {
       email: email,
       password: password,
       returnSecureToken: true,
     });
+    console.log(response.data);
+  } catch (err) {
+    console.log(err.response);
+  }
+}
+
+export async function createUser(email, password) {
+  try {
+    await authenticate(email, password, "signUp");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function login(email, password) {
+  try {
+    await authenticate(email, password);
   } catch (err) {
     console.log(err);
   }
